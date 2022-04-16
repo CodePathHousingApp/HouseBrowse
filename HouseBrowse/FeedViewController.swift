ca98f5fd144e05b86b5333c8fa2401d24c958b95
HuiyuGuan <Huiyu.guan20@bcmail.cuny.edu>
2022-04-16 01:38:38 -0400

    connect the API

--- HouseBrowse/FeedViewController.swift
+++ HouseBrowse/FeedViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 import Parse
+import Foundation
 
 class FeedViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var houses = [[String: Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let headers = [
+            "X-RapidAPI-Host": "us-real-estate.p.rapidapi.com",
+            "X-RapidAPI-Key": "70ee5bc02cmsh918bd04c9a9d8b1p1c5f94jsnbd17f2851ba5"
+        ]
+
+        let request = NSMutableURLRequest(url: NSURL(string: "https://us-real-estate.p.rapidapi.com/v2/property-detail?property_id=3199790641")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse)
+            }
+        })
+
+        dataTask.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return houses.count
     }
     
     @IBAction func onLogoutButton(_ sender: Any) {
@@ -25,6 +56,41 @@ class FeedViewController: UIViewController {
                 delegate.window?.rootViewController = loginViewController
     }
     
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "HouseCell") as! HouseCell
+//
+//        let house = houses[indexPath.row]
+//        let title = house["title"] as! String
+//        let synopsis = house["overview"] as! String
+//
+//
+//        cell.titleLabel.text = title
+//        cell.synopsisLabel.text = synopsis
+//
+//        let baseUrl = "http://image.tmdb.org/t/p/w185"
+//        let posterPath = movie["poster_path"] as! String
+//        let posterUrl = URL(string:baseUrl + posterPath)
+//
+//        cell.posterView.af.setImage(withURL: posterUrl!)
+//
+//        return cell
+//    }
+//
+//    override func prepare(for segue:
+//        UIStoryboardSegue, sender: Any?){
+//
+//        print("Loading up the details screen")
+//        //Find the selected movie
+//        let cell = sender as! UITableViewCell
+//        let indexPath = tableView.indexPath(for: cell)!
+//        let movie = movies[indexPath.row]
+//        //pass the selected movie to the details view controller
+//
+//        let detailViewController = segue.destination as! MovieDetailsViewController
+//        detailViewController.movie = movie
+//
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
     /*
     // MARK: - Navigation
 

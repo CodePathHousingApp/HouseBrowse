930d214a31453ab56115983c505f629bd4ac2a7a
cristiang2 <cristiangue32@gmail.com>
2022-04-23 03:01:45 -0400

    fetching the data for home page

--- HouseBrowse/FeedViewController.swift
+++ HouseBrowse/FeedViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 import Parse
 import Foundation
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -18,13 +20,16 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         // Do any additional setup after loading the view.
         let headers = [
             "X-RapidAPI-Host": "us-real-estate.p.rapidapi.com",
             "X-RapidAPI-Key": "70ee5bc02cmsh918bd04c9a9d8b1p1c5f94jsnbd17f2851ba5"
         ]
 
-        let request = NSMutableURLRequest(url: NSURL(string: "https://us-real-estate.p.rapidapi.com/v2/property-detail?property_id=3199790641")! as URL,
+        let request = NSMutableURLRequest(url: NSURL(string: "https://us-real-estate.p.rapidapi.com/for-sale?offset=0&limit=5&state_code=NY&city=Jamaica&sort=newest")! as URL,
                                                 cachePolicy: .useProtocolCachePolicy,
                                             timeoutInterval: 10.0)
         request.httpMethod = "GET"
@@ -33,9 +38,15 @@ class FeedViewController: UIViewController {
         let session = URLSession.shared
         let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
             if (error != nil) {
-                print(error)
-            } else {
+                print(error!)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
                 let httpResponse = response as? HTTPURLResponse
+                
+//                self.houses = dataDictionary["data"] as! [[String: Any]]
+                
+                print(dataDictionary)
                 print(httpResponse)
             }
         })
@@ -56,25 +67,32 @@ class FeedViewController: UIViewController {
                 delegate.window?.rootViewController = loginViewController
     }
     
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "HouseCell") as! HouseCell
-//
-//        let house = houses[indexPath.row]
-//        let title = house["title"] as! String
-//        let synopsis = house["overview"] as! String
-//
-//
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "HouseCell") as! HouseCell
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HouseCell") as! HouseCell
+        
+        let house = houses[indexPath.row]
+        let title = house["street_name"] as! String
+        let synopsis = house["overview"] as! String
+
+        cell.titleLabel.text = title
+//        cell.textLabel!.text = "row: \(indexPath.row)"
+        
+
+       
+
 //        cell.titleLabel.text = title
 //        cell.synopsisLabel.text = synopsis
-//
-//        let baseUrl = "http://image.tmdb.org/t/p/w185"
-//        let posterPath = movie["poster_path"] as! String
-//        let posterUrl = URL(string:baseUrl + posterPath)
-//
-//        cell.posterView.af.setImage(withURL: posterUrl!)
-//
-//        return cell
-//    }
+
+        //let baseUrl = "http://image.tmdb.org/t/p/w185"
+        //let posterPath = movie["poster_path"] as! String
+        //let posterUrl = URL(string:baseUrl + posterPath)
+
+        //cell.posterView.af.setImage(withURL: posterUrl!)
+
+        return cell
+    }
 //
 //    override func prepare(for segue:
 //        UIStoryboardSegue, sender: Any?){
---
+++ HouseBrowse/HouseCell.swift
@@ -0,0 +1,28 @@
+//
+//  HouseCell.swift
+//  HouseBrowse
+//
+//  Created by Cristian Guerrero on 4/23/22.
+//
+
+import UIKit
+
+class HouseCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
